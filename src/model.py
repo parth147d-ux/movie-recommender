9492712f5b2d511197569240d92b908f38aced19
parth147d-ux <parth147d@gmail.com>
2026-09-18 19:25:51 +0530

    Add caching to create_model function

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+@st.cache_resource
 def create_model(df):
     cv = CountVectorizer(max_features=5000, stop_words='english')
     vectors = cv.fit_transform(df['tags']).toarray()
